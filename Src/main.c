c35021a8c7425b45b04a62a7993d95a9dedb9f5a
Ashton Snelgrove <ashton@snelgrove.science>
2025-01-10 16:31:33 -0700

    Add lab main functions into the header and add a preprocessor switch.

--- Src/main.c
+++ Src/main.c
@@ -3,10 +3,22 @@
 
 int main(void)
 {
-  HAL_Init();
-  SystemClock_Config();
+  #if defined(LAB1)
+  lab1_main();
+  #elif defined(LAB2)
+  lab2_main();
+  #elif defined(LAB3)
+  lab3_main();
+  #elif defined(LAB4)
+  lab4_main();
+  #elif defined(LAB5)
+  lab5_main();
+  #elif defined(LAB6)
+  lab6_main();
+  #elif defined(LAB7)
+  lab7_main();
+  #else
+  #error No valid target specified
+  #endif
 
-  while (1)
-  {
-  }
 }
