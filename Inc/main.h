706195b1d0c38f38aa7c8d0b11037dcf4977813f
Ashton Snelgrove <ashton@snelgrove.science>
2025-01-27 16:45:44 -0700

    Add missing definitions of lab main functrions in main header

--- Inc/main.h
+++ Inc/main.h
@@ -2,3 +2,10 @@
 #include <stdint.h>
 
 void SystemClock_Config(void);
+int lab1_main(void);
+int lab2_main(void);
+int lab3_main(void);
+int lab4_main(void);
+int lab5_main(void);
+int lab6_main(void);
+int lab7_main(void);
